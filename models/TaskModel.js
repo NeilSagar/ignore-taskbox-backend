a2c6c6b9bff0f2f921365169e049c08225fbab07
Neil Sagar Sahu <neilsagar55555@gmail.com>
2024-02-26 16:23:31 +0530

    changes for complete status

--- models/TaskModel.js
+++ models/TaskModel.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const taskSchema=new mongoose.Schema({
-    task_name:String
+    task_name:String,
+    completed:{
+        type:Boolean,
+        default:false,
+    },
 });
 
 const taskModel=mongoose.model("Task",taskSchema);
